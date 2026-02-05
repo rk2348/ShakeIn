1f9572f1646a792ba2c0e6b7096937a857808738
ruito0341 <shiun.rai.0341@gmail.com>
2026-02-05 15:02:19 +0900

    update

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -1,22 +1,38 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class RespawnPoint : MonoBehaviour
 {
     public float deadLineY = -2.5f;
-
     public Transform respawnPosition;
+    public int maxRespawn = 2;
 
+    int respawnCount = 0;
     void Update()
     {
-        if(transform.position.y < deadLineY)
+        if(Keyboard.current.enterKey.wasPressedThisFrame)
         {
-            Respawn();
+            HandlRespawn();
         }
     }
 
-    void Respawn()
+    void HandlRespawn()
     {
+        respawnCount++;
+
+        if(respawnCount > maxRespawn)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if(rb != null)
+        {
+        rb.linearVelocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        }
+        
         transform.position = respawnPosition.position;
+        
     }
 
 }
